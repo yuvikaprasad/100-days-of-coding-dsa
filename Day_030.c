22c492762872547e0ec38c4f70eb3e9b09736ca6
yuvikaprasad <223606297+yuvikaprasad@users.noreply.github.com>
2026-03-02 17:00:57 +0530

    added day 30

---
+++ Day_030.c
@@ -0,0 +1,69 @@
+// Problem: Polynomial Using Linked List - Implement using linked list with dynamic memory allocation.
+
+// Input:
+// - First line: integer n (number of terms)
+// - Next n lines: two integers (coefficient and exponent)
+
+// Output:
+// - Print polynomial in standard form, e.g., 10x^4 + 20x^3 + 30x^2 + 40x + 50
+
+// Example:
+// Input:
+// 5
+// 10 4
+// 20 3
+// 30 2
+// 40 1
+// 50 0
+
+// Output:
+// 10x^4 + 20x^3 + 30x^2 + 40x + 50
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *tail = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int c, e;
+        scanf("%d %d", &c, &e);
+
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->coeff = c;
+        newNode->exp = e;
+        newNode->next = NULL;
+
+        if (!head) head = tail = newNode;
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    struct Node* temp = head;
+    while (temp) {
+        if (temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if (temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+
+        if (temp->next)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+
+    return 0;
+}
